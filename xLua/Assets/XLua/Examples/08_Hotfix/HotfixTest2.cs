0d6efa7a49d7aa8dcef91d121c4a83ed85d01189
bianchui <bianchui@gmail.com>
2022-04-07 17:17:36 +0800

    test hotfix

--- xLua/Assets/XLua/Examples/08_Hotfix/HotfixTest2.cs
+++ xLua/Assets/XLua/Examples/08_Hotfix/HotfixTest2.cs
@@ -1,205 +1,165 @@
 ﻿using UnityEngine;
 using XLua;
 
-namespace XLuaTest
-{
-    [Hotfix]
-    public class HotfixCalc
-    {
-        public int Add(int a, int b)
-        {
+namespace XLuaTest {
+    //[Hotfix]
+    public class HotfixCalc {
+        public int Add(int a, int b) {
             return a - b;
         }
 
-        public Vector3 Add(Vector3 a, Vector3 b)
-        {
+        public Vector3 Add(Vector3 a, Vector3 b) {
             return a - b;
         }
 
-        public int TestOut(int a, out double b, ref string c)
-        {
+        public int TestOut(int a, out double b, ref string c) {
             b = a + 2;
             c = "wrong version";
             return a + 3;
         }
 
-        public int TestOut(int a, out double b, ref string c, GameObject go)
-        {
+        public int TestOut(int a, out double b, ref string c, GameObject go) {
             return TestOut(a, out b, ref c);
         }
 
-        public T Test1<T>()
-        {
+        public T Test1<T>() {
             return default(T);
         }
 
-        public T1 Test2<T1, T2, T3>(T1 a, out T2 b, ref T3 c)
-        {
+        public T1 Test2<T1, T2, T3>(T1 a, out T2 b, ref T3 c) {
             b = default(T2);
             return a;
         }
 
-        public static int Test3<T>(T a)
-        {
+        public static int Test3<T>(T a) {
             return 0;
         }
 
-        public static void Test4<T>(T a)
-        {
+        public static void Test4<T>(T a) {
         }
 
-        public void Test5<T>(int a, params T[] arg)
-        {
+        public void Test5<T>(int a, params T[] arg) {
 
         }
     }
 
-    public class NoHotfixCalc
-    {
-        public int Add(int a, int b)
-        {
+    public class NoHotfixCalc {
+        public int Add(int a, int b) {
             return a + b;
         }
     }
 
-    [Hotfix]
-    public class GenericClass<T>
-    {
+    //[Hotfix]
+    public class GenericClass<T> {
         T a;
 
-        public GenericClass(T a)
-        {
+        public GenericClass(T a) {
             this.a = a;
         }
 
-        public void Func1()
-        {
+        public void Func1() {
             Debug.Log("a=" + a);
         }
 
-        public T Func2()
-        {
+        public T Func2() {
             return default(T);
         }
     }
 
-    [Hotfix]
-    public class InnerTypeTest
-    {
-        public void Foo()
-        {
+    //[Hotfix]
+    public class InnerTypeTest {
+        public void Foo() {
             _InnerStruct ret = Bar();
             Debug.Log("{x=" + ret.x + ",y= " + ret.y + "}");
         }
 
-        struct _InnerStruct
-        {
+        struct _InnerStruct {
             public int x;
             public int y;
         }
 
-        _InnerStruct Bar()
-        {
+        _InnerStruct Bar() {
             return new _InnerStruct { x = 1, y = 2 };
         }
     }
 
-    public class BaseTestHelper
-    {
+    public class BaseTestHelper {
 
     }
 
-    public class BaseTestBase<T> : BaseTestHelper
-    {
-        public virtual void Foo(int p)
-        {
+    public class BaseTestBase<T> : BaseTestHelper {
+        public virtual void Foo(int p) {
             Debug.Log("BaseTestBase<>.Foo, p = " + p);
         }
     }
 
-    [Hotfix]
+    //[Hotfix]
     [LuaCallCSharp]
-    public class BaseTest : BaseTestBase<InnerTypeTest>
-    {
-        public override void Foo(int p)
-        {
+    public class BaseTest : BaseTestBase<InnerTypeTest> {
+        public override void Foo(int p) {
             Debug.Log("BaseTest<>.Foo, p = " + p);
         }
 
-        public void Proxy(int p)
-        {
+        public void Proxy(int p) {
             base.Foo(p);
         }
 
-        public override string ToString()
-        {
+        public override string ToString() {
             return base.ToString();
         }
     }
 
-    [Hotfix]
-    public struct StructTest
-    {
+    //[Hotfix]
+    public struct StructTest {
         GameObject go;
-        public StructTest(GameObject go)
-        {
+        public StructTest(GameObject go) {
             this.go = go;
         }
 
-        public GameObject GetGo(int a, object b)
-        {
+        public GameObject GetGo(int a, object b) {
             return go;
         }
 
-        public override string ToString()
-        {
+        public override string ToString() {
             return base.ToString();
         }
 
-        public string Proxy()
-        {
+        public string Proxy() {
             return base.ToString();
         }
     }
 
-    [Hotfix]
-    public struct GenericStruct<T>
-    {
+    //[Hotfix]
+    public struct GenericStruct<T> {
         T a;
 
-        public GenericStruct(T a)
-        {
+        public GenericStruct(T a) {
             this.a = a;
         }
 
-        public T GetA(int p)
-        {
+        public T GetA(int p) {
             return a;
         }
     }
 
-    public class HotfixTest2 : MonoBehaviour
-    {
+    public class HotfixTest2 : MonoBehaviour {
 
         // Use this for initialization
-        void Start()
-        {
+        void Start() {
             LuaEnv luaenv = new LuaEnv();
             HotfixCalc calc = new HotfixCalc();
             NoHotfixCalc ordinaryCalc = new NoHotfixCalc();
 
             int CALL_TIME = 100 * 1000 * 1000;
             var start = System.DateTime.Now;
-            for (int i = 0; i < CALL_TIME; i++)
-            {
+            for (int i = 0; i < CALL_TIME; i++) {
                 calc.Add(2, 1);
             }
             var d1 = (System.DateTime.Now - start).TotalMilliseconds;
             Debug.Log("Hotfix using:" + d1);
 
             start = System.DateTime.Now;
-            for (int i = 0; i < CALL_TIME; i++)
-            {
+            for (int i = 0; i < CALL_TIME; i++) {
                 ordinaryCalc.Add(2, 1);
             }
             var d2 = (System.DateTime.Now - start).TotalMilliseconds;
@@ -386,12 +346,9 @@ namespace XLuaTest
         ");
             Debug.Log("gs.GetA()=" + gs.GetA(123));
 
-            try
-            {
+            try {
                 calc.TestOut(100, out num, ref str, gameObject);
-            }
-            catch (LuaException e)
-            {
+            } catch (LuaException e) {
                 Debug.Log("throw in lua an catch in c# ok, e.Message:" + e.Message);
             }
 
@@ -412,15 +369,13 @@ namespace XLuaTest
             Debug.Log(bt);
         }
 
-        void TestStateful()
-        {
+        void TestStateful() {
             StatefullTest sft = new StatefullTest();
             sft.AProp = 10;
             Debug.Log("sft.AProp:" + sft.AProp);
             sft["1"] = 1;
             Debug.Log("sft['1']:" + sft["1"]);
-            System.Action<int, double> cb = (a, b) =>
-            {
+            System.Action<int, double> cb = (a, b) => {
                 Debug.Log("a:" + a + ",b:" + b);
             };
             sft.AEvent += cb;
@@ -435,8 +390,7 @@ namespace XLuaTest
         }
 
         // Update is called once per frame
-        void Update()
-        {
+        void Update() {
 
         }
     }
