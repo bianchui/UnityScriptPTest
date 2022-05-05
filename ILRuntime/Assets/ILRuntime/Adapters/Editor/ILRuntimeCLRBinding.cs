dae29928e9fc6e84b820e46bafcffacde46ad9b1
bianchui <bianchui@gmail.com>
2022-05-05 19:50:16 +0800

    fix Generator

--- ILRuntime/Assets/ILRuntime/Adapters/Editor/ILRuntimeCLRBinding.cs
+++ ILRuntime/Assets/ILRuntime/Adapters/Editor/ILRuntimeCLRBinding.cs
@@ -4,22 +4,49 @@ using UnityEngine;
 using System;
 using System.Text;
 using System.Collections.Generic;
-using ILRuntimeDemo;
 [System.Reflection.Obfuscation(Exclude = true)]
 public class ILRuntimeCLRBinding
 {
+        
+    [MenuItem("ILRuntime/Generate CLR Binding Code")]
+    static void GenerateCLRBinding()
+    {
+        List<Type> types = new List<Type>();
+        types.Add(typeof(int));
+        types.Add(typeof(float));
+        types.Add(typeof(long));
+        types.Add(typeof(object));
+        types.Add(typeof(string));
+        types.Add(typeof(Array));
+        types.Add(typeof(Vector2));
+        types.Add(typeof(Vector3));
+        types.Add(typeof(Quaternion));
+        types.Add(typeof(GameObject));
+        types.Add(typeof(UnityEngine.Object));
+        types.Add(typeof(SkinnedMeshRenderer));
+        types.Add(typeof(Input));
+        types.Add(typeof(Transform));
+        types.Add(typeof(RectTransform));
+        types.Add(typeof(Time));
+        types.Add(typeof(UserClass));
+        types.Add(typeof(Debug));
+        //所有DLL内的类型的真实C#类型都是ILTypeInstance
+        types.Add(typeof(List<ILRuntime.Runtime.Intepreter.ILTypeInstance>));
+        ILRuntime.Runtime.CLRBinding.BindingCodeGenerator.GenerateBindingCode(types, "Assets/ILRuntime/Generated");
+    }
+
    [MenuItem("ILRuntime/通过自动分析热更DLL生成CLR绑定")]
     static void GenerateCLRBindingByAnalysis()
     {
         //用新的分析热更dll调用引用来生成绑定代码
         ILRuntime.Runtime.Enviorment.AppDomain domain = new ILRuntime.Runtime.Enviorment.AppDomain();
-        using (System.IO.FileStream fs = new System.IO.FileStream("Assets/StreamingAssets/HotFix_Project.dll", System.IO.FileMode.Open, System.IO.FileAccess.Read))
+        using (System.IO.FileStream fs = new System.IO.FileStream("Assets/StreamingAssets/PTestDll.dll", System.IO.FileMode.Open, System.IO.FileAccess.Read))
         {
             domain.LoadAssembly(fs);
 
             //Crossbind Adapter is needed to generate the correct binding code
             InitILRuntime(domain);
-            ILRuntime.Runtime.CLRBinding.BindingCodeGenerator.GenerateBindingCode(domain, "Assets/Samples/ILRuntime/Generated");
+            ILRuntime.Runtime.CLRBinding.BindingCodeGenerator.GenerateBindingCode(domain, "Assets/ILRuntime/Generated");
         }
 
         AssetDatabase.Refresh();
@@ -30,7 +57,6 @@ public class ILRuntimeCLRBinding
         //这里需要注册所有热更DLL中用到的跨域继承Adapter，否则无法正确抓取引用
         domain.RegisterCrossBindingAdaptor(new MonoBehaviourAdapter());
         domain.RegisterCrossBindingAdaptor(new CoroutineAdapter());
-        domain.RegisterCrossBindingAdaptor(new TestClassBaseAdapter());
         domain.RegisterValueTypeBinder(typeof(Vector3), new Vector3Binder());
         domain.RegisterValueTypeBinder(typeof(Vector2), new Vector2Binder());
         domain.RegisterValueTypeBinder(typeof(Quaternion), new QuaternionBinder());
